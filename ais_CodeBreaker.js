f3d3eee582bfd6f1dfc9fef1881a757208266253
jameswmccarty <jameswmccarty@gmail.com>
2022-12-26 19:05:01 -0500

    Add files via upload

---
+++ ais_CodeBreaker.js
@@ -0,0 +1,34 @@
+// to copy/paste into the dev console
+// https://hack.ainfosec.com/
+// Code Breaker
+
+let solved_pass = ['*','*','*','*','*','*','*']
+let found_vals = []
+let all_vals = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*"
+
+function sleep(ms) {
+      return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+for (let i = 0; i < all_vals.length; i++) {
+	let p2 = Promise.resolve((CodeBreaker_submit(all_vals[i].repeat(7))))
+	await sleep(10)
+	p2.then((value) => { if (value != 0) { found_vals.push(all_vals[i]) } } )
+}
+
+await sleep(4000)
+console.log(found_vals)
+
+for (let i = 0; i < found_vals.length; i++) {
+	for (let j = 0; j < 7; j++) {
+		let str = "-------".split('');
+		str[j] = found_vals[i];
+		str = str.join('');
+		let p3 = Promise.resolve((CodeBreaker_submit(str)))
+		await sleep(10)
+		p3.then((value) => { if (value != 0) { solved_pass[j] = found_vals[i] } } )
+	}
+}
+await sleep(4000)
+CodeBreaker_submit(solved_pass.join(''))
+console.log(solved_pass.join(''))
